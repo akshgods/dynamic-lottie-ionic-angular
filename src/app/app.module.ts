720f7871f764e645ea486f1f5a62b423fc17bf2e
akshgods <akshgods@gmail.com>
2019-02-20 17:41:13 +0530

    updated lottie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,25 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LottieAnimationViewModule } from 'ng-lottie';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    LottieAnimationViewModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  public lottieConfig: Object;
+  private anim: any;
+  private animationSpeed: number = 1;
+  number=0;
+  public myLottie=[{
+      path: 'assets/4413-loading-pizza.json',
+      renderer: 'html',
+      autoplay: true,
+      loop: true
+      },{
+        path:  'assets/4432-face-scanning.json',
+        renderer: 'svg',
+        autoplay: true,
+        loop: true
+    },{
+      path:'assets/4456-user-busy.json',
+      renderer: 'svg',
+      autoplay: true,
+      loop: true
+  }];
+  
+  constructor() {
+  }
+ 
+  handleAnimation(anim: any) {
+      this.anim = anim;
   }
 
+  stop() {
+      this.anim.stop();
+  }
+
+  play() {
+      this.anim.play();
+  }
+
+  pause() {
+      this.anim.pause();
+  }
+
+  setSpeed(speed: number) {
+      this.animationSpeed = speed;
+      this.anim.setSpeed(speed);
+  }
+
+  previous(){
+  this.number= this.number>0 ? (this.number-1) :0;
+  }
+
+  next(){
+  this.number= this.number<2 ? (this.number+1) :2;
+  }
 }
